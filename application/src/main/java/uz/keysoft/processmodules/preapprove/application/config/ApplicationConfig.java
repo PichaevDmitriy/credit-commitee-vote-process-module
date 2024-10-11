c98f155d65bcc92970fa18f97f2c81deaced7063
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2024-10-11 14:27:31 +0500

    add application module

---
+++ application/src/main/java/uz/keysoft/processmodules/preapprove/application/config/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package uz.keysoft.processmodules.preapprove.application.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+}
---
+++ application/src/test/java/uz/keysoft/processmodules/preapprove/application/config/TestConfig.java
@@ -0,0 +1,9 @@
+package uz.keysoft.processmodules.preapprove.application.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan("uz.keysoft.processmodules.preapprove.application")
+public class TestConfig {
+}
