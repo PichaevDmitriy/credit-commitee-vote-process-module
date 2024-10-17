80c54d8b9dd1f6da94b0cb2dc5113d9f38547f6c
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2024-10-17 12:46:37 +0500

    more good-looking

--- jpa-postgres-adapter/src/main/java/uz/keysoft/processmodules/jpapostgresadapter/entity/imps/CustomerEntity.java
+++ jpa-postgres-adapter/src/main/java/uz/keysoft/processmodules/jpapostgresadapter/entity/imps/CustomerEntity.java
@@ -2,9 +2,6 @@ package uz.keysoft.processmodules.jpapostgresadapter.entity.imps;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,10 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import uz.keysoft.processmodules.jpapostgresadapter.entity.AbstractEntity;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Data
@@ -27,29 +22,19 @@ import java.util.Objects;
 @AllArgsConstructor
 @Table(name = "CUSTOMER")
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CustomerEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  Long id;
+public class CustomerEntity extends AbstractEntity {
+
   @Column(name = "FIRST_NAME")
   String firstName;
   @Column(name = "LAST_NAME")
   String lastName;
-
-  @CreationTimestamp
-  @Column(name = "CREATED_DATE", updatable = false)
-  LocalDateTime createdDate;
-  @UpdateTimestamp
-  @Column(name = "UPDATED_DATE", insertable = false)
-  LocalDateTime updatedDate;
-  boolean isDeleted;
 
   @Override
   public final boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof CustomerEntity that)) return false;
 
-    return isDeleted() == that.isDeleted() && Objects.equals(getId(), that.getId()) && Objects.equals(getFirstName(), that.getFirstName()) && Objects.equals(getLastName(), that.getLastName()) && Objects.equals(getCreatedDate(), that.getCreatedDate());
+    return Objects.equals(getId(), that.getId()) && Objects.equals(getFirstName(), that.getFirstName()) && Objects.equals(getLastName(), that.getLastName());
   }
 
   @Override
@@ -57,8 +42,6 @@ public class CustomerEntity {
     int result = Objects.hashCode(getId());
     result = 31 * result + Objects.hashCode(getFirstName());
     result = 31 * result + Objects.hashCode(getLastName());
-    result = 31 * result + Objects.hashCode(getCreatedDate());
-    result = 31 * result + Boolean.hashCode(isDeleted());
     return result;
   }
 }
