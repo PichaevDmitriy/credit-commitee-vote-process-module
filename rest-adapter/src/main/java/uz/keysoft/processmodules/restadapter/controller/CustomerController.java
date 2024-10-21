01c247d7273699c9c55b82830913e33715498029
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2024-10-21 17:25:57 +0500

    add rest adapter

---
+++ rest-adapter/src/main/java/uz/keysoft/processmodules/restadapter/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package uz.keysoft.processmodules.restadapter.controller;
+
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.keysoft.processmodules.domain.model.Customer;
+import uz.keysoft.processmodules.domain.repository.CustomerRepositoryService;
+import uz.keysoft.processmodules.restadapter.dto.CustomerApi;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/customer")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CustomerController {
+  CustomerRepositoryService customerRepositoryServiceService;
+  ModelMapper modelMapper;
+
+  @PostMapping("/create")
+  public ResponseEntity<CustomerApi> createCustomer(@RequestBody final CustomerApi customer) {
+    final Customer customerModel = modelMapper.map(customer, Customer.class);
+    final Customer savedCustomer = customerRepositoryServiceService.save(customerModel);
+    final CustomerApi result = modelMapper.map(savedCustomer, CustomerApi.class);
+    return ResponseEntity.ok(result);
+  }
+}
--- rest-adapter/src/test/java/uz/keysoft/processmodules/restadapter/config/RestAdapterTestConfig.java
+++ rest-adapter/src/test/java/uz/keysoft/processmodules/restadapter/config/RestAdapterTestConfig.java
@@ -1,17 +1,15 @@
-package uz.keysoft.processmodules.preapprove.config;
+package uz.keysoft.processmodules.restadapter.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @AllArgsConstructor
-@ComponentScan("uz.keysoft.processmodules")
-@EnableJpaRepositories(basePackages="uz.keysoft.processmodules")
-public class TestConfig {
+@ComponentScan("uz.keysoft.processmodules.restadapter")
+public class RestAdapterTestConfig {
 }
 
